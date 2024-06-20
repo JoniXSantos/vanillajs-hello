4080070f45c2d0c5072e2639634656e60ef2b46b
Joni Santos <jonisantos1996@hotmail.com>
2024-06-20 16:42:33 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,57 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = [
+  "An alien",
+  "A bad worker",
+  "The enemy",
+  "Something",
+  "An entity",
+  "A virus"
+];
+let action = [
+  "took control of",
+  "messed with",
+  "hid",
+  "interfired with",
+  "destroyed",
+  "destabilised"
+];
+let what = [
+  "this webpage",
+  "the data",
+  "the server",
+  "the system",
+  "the mainframe",
+  "the firewalls"
+];
+let when = [
+  "just now",
+  "last week",
+  "yesterday",
+  "a few days ago",
+  "in May",
+  "on New Year's day"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function GetRandomElement(array) {
+  let randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+function GetRandomExcuse() {
+  let subject = GetRandomElement(who);
+  let verb = GetRandomElement(action);
+  let object = GetRandomElement(what);
+  let time = GetRandomElement(when);
+
+  let excuse = `${subject} ${verb} ${object} ${time}.`;
+  return excuse;
+}
+
+function UpdateExcuse() {
+  let randomExcuse = GetRandomExcuse();
+  let excuseElement = document.getElementById("excuse");
+  excuseElement.textContent = randomExcuse;
+}
+
+window.onload = UpdateExcuse;

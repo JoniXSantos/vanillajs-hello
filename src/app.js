dba5681b631533546eb2d158a9eb98acb7915604
Joni Santos <jonisantos1996@hotmail.com>
2024-08-05 18:29:33 +0000

    reorganized

--- src/app.js
+++ src/app.js
@@ -1,57 +1,57 @@
 /* eslint-disable */
-
-let who = [
-  "An alien",
-  "A bad worker",
-  "The enemy",
-  "Something",
-  "An entity",
-  "A virus"
-];
-let action = [
-  "took control of",
-  "messed with",
-  "hid",
-  "interfired with",
-  "destroyed",
-  "destabilised"
-];
-let what = [
-  "this webpage",
-  "the data",
-  "the server",
-  "the system",
-  "the mainframe",
-  "the firewalls"
-];
-let when = [
-  "just now",
-  "last week",
-  "yesterday",
-  "a few days ago",
-  "in May",
-  "on New Year's day"
-];
-
-function GetRandomElement(array) {
+function getRandomElement(array) {
   let randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
 }
 
-function GetRandomExcuse() {
-  let subject = GetRandomElement(who);
-  let verb = GetRandomElement(action);
-  let object = GetRandomElement(what);
-  let time = GetRandomElement(when);
 
+function getRandomExcuse() {
+  let who = [
+    "An alien",
+    "A bad worker",
+    "The enemy",
+    "Something",
+    "An entity",
+    "A virus"
+  ];
+  let action = [
+    "took control of",
+    "messed with",
+    "hid",
+    "interfired with",
+    "destroyed",
+    "destabilised"
+  ];
+  let what = [
+    "this webpage",
+    "the data",
+    "the server",
+    "the system",
+    "the mainframe",
+    "the firewalls"
+  ];
+  let when = [
+    "just now",
+    "last week",
+    "yesterday",
+    "a few days ago",
+    "in May",
+    "on New Year's day"
+  ];
+  let subject = getRandomElement(who);
+  let verb = getRandomElement(action);
+  let object = getRandomElement(what);
+  let time = getRandomElement(when);
   let excuse = `${subject} ${verb} ${object} ${time}.`;
   return excuse;
 }
 
-function UpdateExcuse() {
-  let randomExcuse = GetRandomExcuse();
+
+function updateExcuse() {
+  let randomExcuse = getRandomExcuse();
   let excuseElement = document.getElementById("excuse");
-  excuseElement.textContent = randomExcuse;
+  excuseElement.innerHTML = randomExcuse;
 }
 
-window.onload = UpdateExcuse;
+
+window.onload = updateExcuse();
